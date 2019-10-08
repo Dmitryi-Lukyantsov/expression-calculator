f2dbf4b41121cc4ce96ebd419e2a464921ace28e
Dima Lukyantsov <dmitruxa1@mail.ru>
2019-10-09 02:32:42 +0300

    '37'

--- src/index.js
+++ src/index.js
@@ -49,49 +49,67 @@ function expressionCalculator(expr) {
         return result;
     }
 
-    for (let i = 0; i < x.length; ) {
+    const repeatOnePriority = (x) => {
+        for (let i = 0; i < x.length; ) {
 
-        if (x[i] === '(' && x[i+2] === ')') {
-            x.splice(i, 3, x[i+1] );
-            i = 0;
-        } else if ( x[i+1] === '*' && x[i+2] !== '(' && x[i] !== ')' ||
-                    x[i+1] === '/' && x[i+2] !== '(' && x[i] !== '(') { 
+            if ( x[i+1] === '*' && x[i+2] !== '(' && x[i] !== ')' && x[i-1] !== '*' && x[i-1] !== '/' ||
+                x[i+1] === '/' && x[i+2] !== '(' && x[i] !== '(' && x[i-1] !== '*' && x[i-1] !== '/' ) { 
 
-                    y = methods[x[i+1]](x[i], x[i+2]);
-                    x.splice(i, 3, y );
-                    i = 0;
-        } else {i++}
+                y = methods[x[i+1]](x[i], x[i+2]);
+                x.splice(i, 3, y );
+                i = 0;
+            } else if (x[i] === '(' && x[i+2] === ')') {
+                x.splice(i, 3, x[i+1] );
+                i = 0;
+            } else {i++}
+        }
     }
 
-    for (let i = 0; i < x.length; ) {
-        if (x[i] === '(' && x[i+2] === ')') {
-        x.splice(i, 3, x[i+1] );
-        i = 0;
-        } else if ( x[i+1] === '+' && x[i+2] !== '(' && x[i] !== ')' ||
-             x[i+1] === '-' && x[i+2] !== '(' && x[i] !== '(') { 
-            
-            y = methods[x[i+1]](+x[i], +x[i+2]);
-            x.splice(i, 3, y );
-            i = 0;
-        } else {i++}
+    const repeatTwoPriority = (x) => {
+        for (let i = 0; i < x.length; ) {
+
+            if ( x[i+1] === '+' && x[i+2] !== '(' && x[i] !== ')' && x[i-1] !== '*' && x[i-1] !== '/' && x[i-1] !== '+' && x[i-1] !== '-'|| 
+                 x[i+1] === '-' && x[i+2] !== '(' && x[i] !== '(' && x[i-1] !== '*' && x[i-1] !== '/' && x[i-1] !== '+' && x[i-1] !== '-' ) { 
+                
+                y = methods[x[i+1]](+x[i], +x[i+2]);
+                x.splice(i, 3, y );
+                i = 0;
+            } else if (x[i] === '(' && x[i+2] === ')') {
+                x.splice(i, 3, x[i+1] );
+                i = 0;
+            } else {i++}
+        }
     }
+
+    // repeatOnePriority(x);
+    // repeatTwoPriority(x)
+    // repeatOnePriority(x);
+    // repeatTwoPriority(x)
+    // repeatOnePriority(x);
+    // repeatTwoPriority(x)
+    
 
     for (let i = 0; i < x.length; i++) {
+        repeatOnePriority(x);
+        repeatTwoPriority(x);
+
+        // if (x.length > 2) {i=0}
         if (x.length === 1) {
             result = x[0];
             console.log(result)
             return result;
-        }
+        } else { i++ }
     }
 
-    
-console.log(expStr)
+    let q = x.join('')
+    console.log(expStr)
+    console.log(q)
     console.log(x)
 }
 
 
 expressionCalculator(" 59 - 13 + (  25 * 22 / (  47 / 38 * (  64 / 93 - 91 + 72  ) * 66  ) + 43 - 5  ) * 39 / 55 ")
-expressionCalculator(" 20 - 57 * 12 - (  58 + 84 * 32 / 27  ) ")
+expressionCalculator(" (  96 / 83 - 53 - (  59 - 91 / 91 - 54  )  ) / (  75 + 4 / (  50 - 80 * 45 + 93 + 18  ) - 76 / 54  ) * 14 + 59  ")
 
 
 module.exports = {
